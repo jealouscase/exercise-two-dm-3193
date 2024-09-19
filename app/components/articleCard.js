ef9c12278d3c02bc169e8353265b821bdd8864b4
jealouscase <henryosterweis@gmail.com>
2024-09-19 09:18:10 -0400

    changes

---
+++ app/components/articleCard.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ArticleCard({ article }) {
+    return (
+        <div className="border border-black w-full flex p-4 gap-6">
+            <Image 
+                src={article.image.url} 
+                alt={article.image.alt} 
+                width={300} 
+                height={200} 
+            />
+            <div className="flex flex-col justify-between items-start gap-2">
+                <h2 className="text-2xl font-bold">{article.id}</h2>
+                <h3>{article.publishedDate}</h3>
+                <p>{article.articleText[0].data}</p>
+                <Link href={`article/${article.id}`} className="font-bold text-blue-700">Read More</Link>
+            </div>
+        </div>
+    )
+}
